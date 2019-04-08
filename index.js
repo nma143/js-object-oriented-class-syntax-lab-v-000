6b7b8d427c7dba9643c7e0bbd3f05de3b4f90ac8
Nikki A <nikki.apostolakis@gmail.com>
2019-04-08 21:50:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,3 +14,8 @@ class BoardMember
   sayHi(){return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;};
 
 }
+
+class Ceo extends BoardMember
+{
+  
+}
